e1888934a1c123fd96137b7e00d80baa1c29fa22
PradeepPradeep1234 <pradeeppardeep65@gmail.com>
2026-03-20 08:45:22 -0700

    load env added

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from datetime import datetime, date, timedelta
 from flask_bcrypt import Bcrypt
 from flask import flash
 from flask_migrate import Migrate
+import os
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
 from functools import wraps
 from reportlab.lib.pagesizes import A4
@@ -14,23 +15,28 @@ from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.enums import TA_CENTER, TA_RIGHT, TA_LEFT
 from flask_mail import Mail, Message
 import io
+from dotenv import load_dotenv
 
+
+# Load variables from .env file
+load_dotenv()
 app = Flask(__name__)
-app.config['MAIL_SERVER']   = 'smtp.gmail.com'
-app.config['MAIL_PORT']     = 587
-app.config['MAIL_USE_TLS']  = True
-app.config['MAIL_USE_SSL']  = False
-app.config['MAIL_USERNAME'] = 'pradeeppardeep65@gmail.com'
-app.config['MAIL_PASSWORD'] = 'scacfyggqpplbfte'
+app.config['MAIL_SERVER']   = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
+app.config['MAIL_PORT']     = int(os.environ.get('MAIL_PORT', 587))
+app.config['MAIL_USE_TLS']  = os.environ.get('MAIL_USE_TLS', 'True') == 'True'
+app.config['MAIL_USE_SSL']  = os.environ.get('MAIL_USE_SSL', 'False') == 'True'
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 mail = Mail(app)
 
-app.config['SECRET_KEY']              = 'glowpark@123'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///glowpark.db'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'fallback_key_for_local_dev')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///glowpark.db')
 db     = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 lm     = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
+
 
 ROLE_ADMIN   = 'admin'
 ROLE_MANAGER = 'manager'
